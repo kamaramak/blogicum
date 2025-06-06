62be975460cb2e20d384a4b2aebcf21f2c500c7c
Марат <kabardokov.m@yandex.ru>
2025-06-06 19:58:58 +0300

    Update apps.py

--- blogicum/blog/apps.py
+++ blogicum/blog/apps.py
@@ -5,7 +5,3 @@ class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
     verbose_name = 'Блог'
-
-    def ready(self):
-        from . import signals
-        return super().ready()
